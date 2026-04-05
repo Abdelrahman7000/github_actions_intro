2946b2c410e80e8961ecf50847091fb076792642
abdelrahman shaban <abdelrahman.shaban7000@gmail.com>
2026-04-06 01:19:01 +0200

    updates the NodeJS application

--- src/app.js
+++ src/app.js
@@ -3,10 +3,10 @@ function greet(name){
     return `Hello, ${name}!`;
     }
 
-    module .exports = greet; // Ensure function is exportable
+    module.exports = greet; // Ensure function is exportable
     // if run directly, print output (useful for debugging)
 
     if (require.main === module) {
 
-    console.log(greet('world'));
+    console.log(greet("World"));
     }
--- src/test.sh
+++ src/test.sh
@@ -2,7 +2,7 @@
 
 # src/test.sh
 EXPECTED="Hello, Test!"
-OUTPUT=$(node -e"console.log(require('./src/app')('Test'))")
+OUTPUT=$(node -e "console.log(require('./src/app')('Test'))")
 if [ "$OUTPUT" == "$EXPECTED" ]; then
 echo "Test passed!"
 exit 0
